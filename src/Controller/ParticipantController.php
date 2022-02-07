e645db56aa017cded3a6a56c0aa96699f6e4df62
c7em <Basket44__>
2022-02-07 12:19:30 +0100

    push nav, controller Sortie, Participants et Campus

---
+++ src/Controller/ParticipantController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ParticipantController extends AbstractController
+{
+    /**
+     * @Route("/participant", name="participant")
+     */
+    public function index(): Response
+    {
+        return $this->render('participant/index.html.twig', [
+            'controller_name' => 'ParticipantController',
+        ]);
+    }
+}
