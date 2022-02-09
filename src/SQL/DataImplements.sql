f59608aecfb94edd3d7571437eeb4ad5dfa6b6ea
Luka Chouville <luka.chouville2021@campus-eni.fr>
2022-02-09 18:21:30 +0100

    SQL Script DataImplements

--- src/SQL/DataImplements.sql
+++ src/SQL/DataImplements.sql
@@ -3,6 +3,11 @@ INSERT INTO `campus` (`id`, `nom`) VALUES
 (2, 'Niort'),
 (3, 'Renne');
 
+INSERT INTO `villes` (`id`, `nom`,`code_postal`) VALUES
+(1, 'Nantes',44000),
+(2, 'Niort',79000),
+(3, 'Rennes',35000);
+
 /* Admin mdp Pa$$w0rd */
 INSERT INTO `participant` (`id`, `nom`, `prenom`, `telephone`, `password`, `actif`, `email`, `roles`, `campus_id`) VALUES
 (1, 'Application', 'Admin', '0000000000', '$2y$13$jdW0fksE6eIsPm1GxK6BxeK/KPKVGosXgnY.w1701rbNfuegVH2Py', NULL, 'admin@sortir.fr', '[\"ROLE_ADMIN\"]', 1);
