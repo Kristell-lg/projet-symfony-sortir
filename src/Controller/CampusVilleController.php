717eca06bf297b45748fb2e1968af44c8b8179b0
Luka Chouville <luka.chouville2021@campus-eni.fr>
2022-02-09 14:20:17 +0100

    Modification Ville & Campus

--- src/Controller/CampusVilleController.php
+++ src/Controller/CampusVilleController.php
@@ -19,52 +19,73 @@ class CampusVilleController extends AbstractController
     /**
      * @Route("/campus", name="campusville_campus")
      */
-    public function campusListe(EntityManagerInterface $entityManager, CampusRepository $campusRepository, Request $request): Response
+    public function campusListe(EntityManagerInterface $entityManager, CampusRepository $campusRepository, Request $request, ?campus $pCampus): Response
     {
+        $modif = false;
         $campus = $campusRepository->findAll();
-
-        $campusNew = new Campus();
+        if($pCampus !== null){
+            $campusNew= $pCampus;
+            $modif=true;
+        }
+        else{
+            $campusNew = new Campus();
+        }
         $campusForm = $this->createForm(CampusType::class,$campusNew);
         $campusForm->handleRequest($request);
 
         if($campusForm->isSubmitted() && $campusForm->isValid()){
             $entityManager->persist($campusNew);
             $entityManager->flush();
-            $this->addFlash('success', 'Campus ajouté !');
+            if($modif){
+                $this->addFlash('success', 'Campus modifé !');
+            } else{
+                $this->addFlash('success', 'Campus ajouté !');
+            }
             return $this->redirectToRoute('campusville_campus');
         }
 
         return $this->render('campus/campus.html.twig', [
             'campus'=>$campus,
-            'campusForm'=>$campusForm->createView()
+            'campusForm'=>$campusForm->createView(),
+            'modif'=>$modif
         ]);
     }
 
     /**
      * @Route("/villes", name="campusville_villes")
      */
-    public function villesListe(EntityManagerInterface $entityManager, VillesRepository $villesRepository, Request $request): Response
+    public function villesListe(EntityManagerInterface $entityManager, VillesRepository $villesRepository, Request $request,?Villes $ville): Response
     {
+        $modif = false;
         $villes= $villesRepository->findAll();
+        if($ville !== null){
+            $villesNew= $ville;
+            $modif=true;
+        }
+        else{
+            $villesNew = new Villes();
+        }
 
-        $villesNew = new Villes();
         $villesForm = $this->createForm(VillesType::class,$villesNew);
         $villesForm->handleRequest($request);
 
         if($villesForm->isSubmitted() && $villesForm->isValid()){
             $entityManager->persist($villesNew);
-            $entityManager->flush();
-            $this->addFlash('success', 'Ville ajoutée !');
+            $entityManager->flush();if($modif){
+                $this->addFlash('success', 'Ville modifé !');
+            } else{
+                $this->addFlash('success', 'Ville ajouté !');
+            }
             return $this->redirectToRoute('campusville_villes');
         }
-
         return $this->render('campus/villes.html.twig', [
             'villes'=>$villes,
-            'villesForm'=>$villesForm->createView()
+            'villesForm'=>$villesForm->createView(),
+            'modif'=>$modif
         ]);
     }
     /**
-     * @Route("/villes/{id}", name="campusville_supprimerville")
+     * @Route("/villes/supprimer/{id}", name="campusville_supprimerville")
      */
     public function supprimerVille(int $id,EntityManagerInterface $entityManager, VillesRepository $villesRepository): Response
     {
@@ -76,7 +97,7 @@ class CampusVilleController extends AbstractController
         return $this->redirectToRoute('campusville_villes');
     }
     /**
-     * @Route("/campus/{id}", name="campusville_supprimercampus")
+     * @Route("/campus/supprimer/{id}", name="campusville_supprimercampus")
      */
     public function supprimerCampus(int $id,EntityManagerInterface $entityManager, CampusRepository $campusRepository): Response
     {
@@ -86,5 +107,22 @@ class CampusVilleController extends AbstractController
 
         $this->addFlash('success', 'Campus supprimé !');
         return $this->redirectToRoute('campusville_campus');
+    }
+    /**
+     * @Route("/villes/modifier/{id}", name="campusville_modifierville")
+     */
+    public function modifierVille(int $id,EntityManagerInterface $entityManager, VillesRepository $villesRepository, Request $request): Response
+    {
+        $ville = $villesRepository->find($id);
+        return $this->villesListe($entityManager,$villesRepository,$request,$ville);
+    }
+
+    /**
+     * @Route("/campus/modifier/{id}", name="campusville_modifiercampus")
+     */
+    public function modifierCampus(int $id,EntityManagerInterface $entityManager, CampusRepository $campusRepository, Request $request): Response
+    {
+        $campus = $campusRepository->find($id);
+        return $this->campusListe($entityManager,$campusRepository,$request,$campus);
     }
 }
