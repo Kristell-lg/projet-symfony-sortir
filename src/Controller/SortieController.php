0c869ad275ea95e727973e7baeab9de5a0c4b620
Kristell <leguyader.kristell@gmail.com>
2022-02-09 15:07:59 +0100

    Formulaire sortie ajout détail lieu

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,10 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Lieux;
 use App\Entity\Sorties;
+use App\Entity\Villes;
+use App\Form\LieuxType;
 use App\Form\SortiesType;
+use App\Form\VillesType;
 use App\Repository\CampusRepository;
 use App\Repository\EtatsRepository;
+use App\Repository\LieuxRepository;
 use App\Repository\ParticipantRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -27,13 +32,30 @@ class SortieController extends AbstractController
     /**
      * @Route("/sortie/create", name="sortie_create")
      */
-    public function create(Request $request, CampusRepository $campusRepository, EtatsRepository $etatsRepository,ParticipantRepository $participantRepository, EntityManagerInterface $entityManager): Response
+    public function create(Request $request, LieuxRepository $lieuxRepository, CampusRepository $campusRepository, EtatsRepository $etatsRepository,ParticipantRepository $participantRepository, EntityManagerInterface $entityManager): Response
     {
         $sortie = new Sorties();
         $sortieForm = $this->createForm(SortiesType::class,$sortie);
-
         $sortieForm->handleRequest($request);
 
+        $lieu = new Lieux();
+        $lieuForm = $this->createForm(LieuxType::class,$lieu);
+        $lieuForm->handleRequest($request);
+
+        //Afficher le détail du lieu quand sélectionner
+        if($sortieForm->get('lieu_btn')->isClicked()){
+            $lieu = $lieuxRepository->find($sortieForm->getData()->getLieux()->getId());
+
+            $sortieForm = $this->createForm(SortiesType::class,$sortie);
+            $sortieForm->handleRequest($request);
+
+            return $this->render('/sortie/create.html.twig', [
+                'sortieForm'=>$sortieForm->createView(),
+                'lieu'=>$lieu
+            ]);
+        }
+
+        //Traiter le formulaire et envoyer en base de données
         if($sortieForm->isSubmitted() && $sortieForm->isValid()){
             $campusList = $campusRepository->findAll();
             foreach ($campusList as $c){
@@ -61,7 +83,8 @@ class SortieController extends AbstractController
         }
 
         return $this->render('/sortie/create.html.twig', [
-            'sortieForm'=>$sortieForm->createView()
+            'sortieForm'=>$sortieForm->createView(),
+            'lieu'=>''
         ]);
     }
 }
