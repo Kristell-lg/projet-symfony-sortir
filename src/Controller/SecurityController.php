bd07ba6516476ef378fbdaddeda2d63040160c5f
yayten <mm.21@hotmail.fr>
2022-02-10 09:27:40 +0100

    Ajout de validation avant edit

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -22,8 +22,8 @@ class SecurityController extends AbstractController
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
-
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+
     }
 
     /**
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -7,10 +7,15 @@ use App\Form\RegistrationFormType;
 use App\Repository\ParticipantRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use http\Client\Curl\User;
+use phpDocumentor\Reflection\Types\String_;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
+use function Sodium\add;
 
 /**
  * @Route("/user", name="user_")
@@ -50,13 +55,14 @@ class UserController extends AbstractController
     )
     {
         $participant = $entityManager->getReference('App:Participant', $id);
+       // $participant->password_verify('mon mot de passe', $participant->password);;
+
         $form = $this->createForm(EditFormType::class, $participant);
         $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
+        if ($form->isSubmitted() && $form->isValid())   {
             $entityManager->persist($participant);
             $entityManager->flush();
             $this->addFlash('Success', 'Le profil à bien été modifié!');
-
             return $this->redirectToRoute('main_home', [
                 'id' => $participant->getId(),
             ]);
@@ -68,6 +74,31 @@ class UserController extends AbstractController
                 'registrationForm' => $form->createView(), "participant" => $participant]
 
         );
+    }
+    /**
+     * @Route ("/validation" , name="validation")
+     */
+    public function validation(AuthenticationUtils $authenticationUtils,UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator): Response{
+
+          //  return $this->redirectToRoute('user_validation');
+        //}
+
+
+       /* if (password_verify('participant.password', 'hash')) {
+            echo 'Le mot de passe est valide !';
+        } else {
+            echo 'Le mot de passe est invalide.';
+        }*/
+
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+
+        return $this->render('user/Validation.html.twig', ['last_username' => $lastUsername, 'error' => $error,]);
+
     }
 
 
