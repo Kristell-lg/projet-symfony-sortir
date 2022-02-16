3d1e367493f5d83636af863d994e2eda6c13fc2c
yayten <mm.21@hotmail.fr>
2022-02-16 14:59:28 +0100

    NETTOYAGE V1

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -20,12 +20,18 @@ use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 /**
  * @Route("/user", name="user_")
  */
+
 class UserController extends AbstractController
 {
     /**
      * @Route("/list", name="list")
      */
-    public function list(ParticipantRepository $participantRepository): Response
+
+    //***********************************************************liste des participants*********************************
+
+    public function list(
+        ParticipantRepository $participantRepository
+    ): Response
     {
         $participant = $participantRepository->findAll();
         return $this->render('user/list.html.twig',
@@ -37,7 +43,13 @@ class UserController extends AbstractController
     /**
      * @Route("/profil/{id}", name="profil")
      */
-    public function profil(int $id, ParticipantRepository $participantRepository): Response
+
+    //***********************************************************Profil des participants défini par son ID (BDD)********
+
+    public function profil(
+        int                   $id,
+        ParticipantRepository $participantRepository
+    ): Response
     {
         $participant = $participantRepository->find($id);
 
@@ -48,11 +60,14 @@ class UserController extends AbstractController
      * @Route ("/edit/{id}" , name="edit")
      */
 
-    public function edit(int                    $id,
-                         ParticipantRepository  $participantRepository,
-                         Request                $request,
-                         EntityManagerInterface $entityManager,
-                         UserPasswordHasherInterface $userPasswordHasher
+    //************************************************Modification du profil du participant suivant son ID (BDD)********
+
+    public function edit(
+        int                         $id,
+        ParticipantRepository       $participantRepository,
+        Request                     $request,
+        EntityManagerInterface      $entityManager,
+        UserPasswordHasherInterface $userPasswordHasher
     )
     {
         $participant = $entityManager->getReference('App:Participant', $id);
@@ -62,76 +77,71 @@ class UserController extends AbstractController
         $currentParticipant = $participant;
 
         //conserver le nom de la photo afin de la supprimer du disque si elle a changé
-        if(!empty($participant->getImages())){
+        if (!empty($participant->getImages())) {
             $participantImgNom = $participant->getImages()->getName();
         }
 
-        $form = $this->createForm(EditFormType::class,$participant);
+        $form = $this->createForm(EditFormType::class, $participant);
         $form->handleRequest($request);
-
-
 
 
         if ($form->isSubmitted() && $form->isValid()) {
 
-                //si le mot de passe n'est pas changé et s'il a changé on le hash avant d'envoyer en BDD
-                if(empty($form->get('password')->getData())){
-                    $participant->setPassword(
-                        $userPasswordHasher->hashPassword(
-                            $participant,
-                            $currentPassword
-                        )
-                    );
-                }
-                else{
-                    $participant->setPassword(
-                        $userPasswordHasher->hashPassword(
-                            $participant,
-                            $form->get('password')->getData()
-                        )
-                    );
-                }
+            //si le mot de passe n'est pas changé et s'il a changé on le hash avant d'envoyer en BDD
+            if (empty($form->get('password')->getData())) {
+                $participant->setPassword(
+                    $userPasswordHasher->hashPassword(
+                        $participant,
+                        $currentPassword
+                    )
+                );
+            } else {
+                $participant->setPassword(
+                    $userPasswordHasher->hashPassword(
+                        $participant,
+                        $form->get('password')->getData()
+                    )
+                );
+            }
 
 
+            //Récupération des images transmises
+            $image = $form->get('image')->getData();
 
-                //Récupération des images transmises
-                $image = $form->get('image')->getData();
 
+            if (!empty($image) && filesize($image) < 500000) {
+                //Générer nom de fichier
+                $fichier = md5(uniqid()) . '.' . $image->guessExtension();
 
-                if(!empty($image) && filesize($image)<500000) {
-                    //Générer nom de fichier
-                    $fichier = md5(uniqid()).'.'.$image->guessExtension();
+                //On copie le fichier dans le dossier uploads
+                $image->move(
+                    'uploads/',
+                    $fichier
+                );
 
-                    //On copie le fichier dans le dossier uploads
-                    $image->move(
-                        'uploads/',
-                        $fichier
-                    );
+                //supprimer l'ancienne photo s'il y en a une
+                if (!empty($participantImgNom)) {
+                    unlink('uploads' . '/' . $participantImgNom);
+                    $img = $participant->getImages();
+                    //On stocke image en BDD(son nom)
+                    $img->setName($fichier);
 
-                    //supprimer l'ancienne photo s'il y en a une
-                    if(!empty($participantImgNom)) {
-                        unlink('uploads' . '/' . $participantImgNom);
-                        $img = $participant->getImages();
-                        //On stocke image en BDD(son nom)
-                        $img->setName($fichier);
-
-                    }
-                    else{
-                        $img = new Images();
-                        $img->setName($fichier);
-                        $participant->setImages($img);
-                    }
-
+                } else {
+                    $img = new Images();
+                    $img->setName($fichier);
+                    $participant->setImages($img);
                 }
 
-                $entityManager->persist($participant);
-                $entityManager->flush();
+            }
 
-                $this->addFlash('success', 'Le profil à bien été modifié!');
+            $entityManager->persist($participant);
+            $entityManager->flush();
 
-                return $this->redirectToRoute('user_profil', [
-                    'id' => $participant->getId(),
-                ]);
+            $this->addFlash('success', 'Le profil à bien été modifié!');
+
+            return $this->redirectToRoute('user_profil', [
+                'id' => $participant->getId(),
+            ]);
         }
 
         return $this->render('user/edit.html.twig', [
@@ -143,9 +153,16 @@ class UserController extends AbstractController
     /**
      * @Route ("/validation" , name="validation")
      */
-    public function validation(Request $request, AuthenticationUtils $authenticationUtils, UserPasswordHasherInterface $userPasswordHasher): Response
+
+    //***********************************************************Validation du mot de passe avant l'edit du profil ******************************************************************************************************************************************************
+
+    public function validation(
+        Request                     $request,
+        AuthenticationUtils         $authenticationUtils,
+        UserPasswordHasherInterface $userPasswordHasher
+    ): Response
     {
-        $error ="";
+        $error = "";
         // dernier username pour éviter de renvoyer le username
         $lastUsername = $authenticationUtils->getLastUsername();
 
@@ -153,15 +170,14 @@ class UserController extends AbstractController
         $confirmPasswordForm = $this->createForm(ConfirmPasswordType::class);
         $confirmPasswordForm->handleRequest($request);
 
-        if($confirmPasswordForm->isSubmitted()){
+        if ($confirmPasswordForm->isSubmitted()) {
 
             $currentPassword = $user->getPassword();
             $typedInPassword = $confirmPasswordForm->get('plainPassword')->getData();
 
-            if(!empty($typedInPassword) && password_verify($typedInPassword,$currentPassword)){
-                return $this->redirectToRoute('user_edit',['id'=>$user->getId()]);
-            }
-            else{
+            if (!empty($typedInPassword) && password_verify($typedInPassword, $currentPassword)) {
+                return $this->redirectToRoute('user_edit', ['id' => $user->getId()]);
+            } else {
                 $error = 'Mot de passe incorrect';
             }
 
@@ -170,7 +186,7 @@ class UserController extends AbstractController
         return $this->render('user/validation.html.twig', [
             'last_username' => $lastUsername,
             'error' => $error,
-            'confirmPasswordForm'=> $confirmPasswordForm->createView()]);
+            'confirmPasswordForm' => $confirmPasswordForm->createView()]);
 
     }
 
@@ -178,45 +194,62 @@ class UserController extends AbstractController
     /**
      * @Route ("/gestion" , name="gestion")
      */
-    public function gestion(ParticipantRepository $participantRepository)
+//***********************************************************Gestion des participants *****************************************************************************************************************************************************************************************************
+    public function gestion(
+        ParticipantRepository $participantRepository
+    )
     {
         $participantsList = $participantRepository->findAll();
-        $isAdmin=false;
+        $isAdmin = false;
         return $this->render('user/gestionUser.html.twig', [
-            'participantsList'=>$participantsList,
-            'isAdmin'=>$isAdmin
+            'participantsList' => $participantsList,
+            'isAdmin' => $isAdmin
         ]);
     }
 
     /**
      * @Route ("/gestion/desactiver/{id}" , name="gestion_desactivate")
      */
-    public function gestionDesactivate(int $id, ParticipantRepository $participantRepository, EntityManagerInterface $entityManager)
+    //Désactivation du compte du participant
+    public function gestionDesactivate(
+        int                    $id,
+        ParticipantRepository  $participantRepository,
+        EntityManagerInterface $entityManager
+    )
     {
+        //Recherche du compte du participant à désactiver
         $participant = $participantRepository->find($id);
+        //Désactivation du compte
         $participant->setActif(false);
 
         $entityManager->persist($participant);
         $entityManager->flush();
 
 
-        $this->addFlash("success","Désactivation réussie !");
+        $this->addFlash("success", "Désactivation réussie !");
         return $this->redirectToRoute('user_gestion');
     }
 
     /**
      * @Route ("/gestion/reactivate/{id}" , name="gestion_reactivate")
      */
-    public function gestionReactivate(int $id, ParticipantRepository $participantRepository, EntityManagerInterface $entityManager)
+//***********************************************************Reactivation du compte du participant au préalable désactivé *********************************************************************************************************************************************************
+    public function gestionReactivate(
+        int                    $id,
+        ParticipantRepository  $participantRepository,
+        EntityManagerInterface $entityManager
+    )
     {
+        //Recherche du participant à réactivé via son ID
         $participant = $participantRepository->find($id);
+        //Reactivation du compte
         $participant->setActif(true);
 
         $entityManager->persist($participant);
         $entityManager->flush();
 
 
-        $this->addFlash("success","Réactivation réussie !");
+        $this->addFlash("success", "Réactivation réussie !");
         return $this->redirectToRoute('user_gestion');
     }
 
@@ -224,63 +257,85 @@ class UserController extends AbstractController
     /**
      * @Route ("/gestion/supprimer/{id}" , name="gestion_delete")
      */
+    //***********************************************************Suppression du participant suivant son ID (BDD)*******************************************************
+
     public function gestionDelete(int $id, ParticipantRepository $participantRepository, EntityManagerInterface $entityManager)
     {
+        //Recherche du participant via son ID
         $participant = $participantRepository->find($id);
+        //Suppression du participant
         $entityManager->remove($participant);
         $entityManager->flush();
 
-        $this->addFlash("success","Suppression réussie !");
+        $this->addFlash("success", "Suppression réussie !");
         return $this->redirectToRoute('user_gestion');
     }
 
     /**
      * @Route ("/gestion/grantAdmin/{id}" , name="gestion_grantAdmin")
      */
-    public function grantAdmin(int $id, ParticipantRepository $participantRepository, EntityManagerInterface $entityManager)
+
+    //***********************************************************Gestion du ROLE_ADMIN d'un participant ************************************************************************
+    public function grantAdmin(
+        int                    $id,
+        ParticipantRepository  $participantRepository,
+        EntityManagerInterface $entityManager
+    )
     {
+        //Recherche du participant via son ID
         $participant = $participantRepository->find($id);
-        $participant->setRoles(["ROLE_USER","ROLE_ADMIN"]);
+        //Mise en place du role ADMIN
+        $participant->setRoles(["ROLE_USER", "ROLE_ADMIN"]);
 
         $entityManager->persist($participant);
         $entityManager->flush();
 
-        $this->addFlash("success","Modification des droits réussie !");
+        $this->addFlash("success", "Modification des droits réussie !");
         return $this->redirectToRoute('user_gestion');
     }
 
     /**
      * @Route ("/gestion/stripAdmin/{id}" , name="gestion_stripAdmin")
      */
-    public function stripAdmin(int $id, ParticipantRepository $participantRepository, EntityManagerInterface $entityManager)
+// ********************************************Retrait du role admin à un participant *****************************************************************************************************************************************************************************************
+    public function stripAdmin(
+        int                    $id,
+        ParticipantRepository  $participantRepository,
+        EntityManagerInterface $entityManager
+    )
     {
+        //Recherche du participant via son ID
         $participant = $participantRepository->find($id);
+        //Attribution du role ROLE_USER
         $participant->setRoles(["ROLE_USER"]);
 
         $entityManager->persist($participant);
         $entityManager->flush();
 
-        $this->addFlash("success","Modification des droits réussie !");
+        $this->addFlash("success", "Modification des droits réussie !");
         return $this->redirectToRoute('user_gestion');
     }
-
 
 
     /**
      * @Route ("/supprime/image/{id}" , name="gestion_image")
      */
+
+    //************************************Suppression de l'image de profil du participant via son ID (BDD)***************************************************************************************************************************************************
+
     public function deleteImage(int $id, ImagesRepository $imagesRepository, EntityManagerInterface $entityManager)
     {
+        //Recherche de l'image de profil via l'id du participant
         $image = $imagesRepository->find($id);
 
         $id = $image->getParticipant()->getId();
         $nom = $image->getName();
-
+        //Suppression de l'image
         $entityManager->remove($image);
         $entityManager->flush();
 
-        unlink('uploads'.'/'.$nom);
+        unlink('uploads' . '/' . $nom);
 
-        return $this->redirectToRoute('user_edit',['id'=>$id]);
+        return $this->redirectToRoute('user_edit', ['id' => $id]);
     }
 }
